8ea16eda7878934076b56adf00ec78677d0d249d
Vinicius Gabriel <vinihbieel49@gmail.com>
2023-06-09 20:21:51 -0300

    Field component

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import params from './src/params';
+import Field from './src/components/Field';
 
 export default function App() {
   return (
@@ -10,6 +11,13 @@ export default function App() {
         Tamanho da grade: 
         {`${params.getRowsAmmount()}x${params.getColumnsAmount()}`}
       </Text>
+      <Field />
+      <Field opened />
+      <Field opened nearMines={1} />
+      <Field opened nearMines={2} />
+      <Field opened nearMines={3} />
+      <Field opened nearMines={6} />
+      <Field opened nearMines={7} />
       <StatusBar style="auto" />
     </View>
   );
