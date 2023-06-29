0784d3beed382c3a132478244e5d727f560493ff
Vinicius Gabriel <vinihbieel49@gmail.com>
2023-06-28 21:47:30 -0300

    minefield component

--- App.js
+++ App.js
@@ -1,30 +1,44 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import params from './src/params';
-import Field from './src/components/Field';
+import MineField from './src/components/MineField';
+import { createMinedBoard } from './src/functions';
+import { Component } from 'react';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Iniciando o Mines!</Text>
-      <Text>
-        Tamanho da grade: 
-        {`${params.getRowsAmmount()}x${params.getColumnsAmount()}`}
-      </Text>
-      <Field />
-      <Field opened />
-      <Field opened nearMines={1} />
-      <Field opened nearMines={2} />
-      <Field opened nearMines={3} />
-      <Field opened nearMines={6} />
-      <Field opened nearMines={7} />
-      <Field mined />
-      <Field mined opened />
-      <Field mined opened exploded />
-      <Field flagged />
-      <Field flagged opened />
-    </View>
-  );
+export default class App extends Component {
+  
+  constructor(props) {
+    super(props)
+    this.state = this.createState()
+  }
+  minesAmount = () => {
+    const cols = params.getColumnsAmount()
+    const rows = params.getRowsAmmount()
+    return Math.ceil(cols * rows * params.difficultLevel)
+  }
+
+  createState = () => {
+    const cols = params.getColumnsAmount()
+    const rows = params.getRowsAmmount()
+    return {
+      board: createMinedBoard(rows, cols, this.minesAmount())
+    }
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Iniciando o Mines!</Text>
+        <Text>
+          Tamanho da grade:
+          {`${params.getRowsAmmount()}x${params.getColumnsAmount()}`}
+        </Text>
+        <View style={styles.board}>
+          <MineField board={this.state.board} />
+        </View>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
@@ -32,6 +46,10 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'flex-end',
   },
+  board: {
+    alignItems: 'center',
+    backgroundColor: '#AAA'
+  }
 });
