038d6036fb29bf16333b6faa9a0ebd62f029f1d2
Vinicius Gabriel <vinihbieel49@gmail.com>
2023-06-29 19:57:31 -0300

    user files

--- App.js
+++ App.js
@@ -1,20 +1,40 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { createStackNavigator } from '@react-navigation/stack'
+import UserList from './views/UserList';
+import { NavigationContainer } from '@react-navigation/native'
+import UserForm from './views/UserForm';
+import { Button, Icon } from 'react-native-elements';
 
+const Stack = createStackNavigator()
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="UserList" screenOptions={screenOptions}>
+        <Stack.Screen name="UserList" component={UserList} options={({ navigation }) => {
+          return {
+            title: "Lista de Usuários",
+            headerRight: () => {
+              return (
+                <Button
+                  onPress={() => navigation.navigate("UserForm")}
+                  type="clear"
+                  icon={<Icon name='add' size={25} color='white' />}
+                />)
+
+            }
+          }
+        }} />
+        <Stack.Screen name="UserForm" component={UserForm} options={{ title: "Formulário de Usuários" }} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+const screenOptions = {
+  headerStyle: {
+    backgroundColor: '#f4511e'
   },
-});
+  headerTintColor: '#fff',
+  headerTintStyle: {
+    fontWeight: 'bold'
+  }
+}
---
+++ data/users.js
@@ -0,0 +1,25 @@
+export default [
+  {
+    id: 1,
+    name: 'João Silva',
+    email: 'josil@empmail.com',
+    avatarUrl: 'https://cdn.pixabay.com/photo/2016/11/18/23/38/child-1837375_960_7'
+  },
+  {
+    id: 2,
+    name: 'Mariana Aguiar', email: 'riamasil@empmail.com',
+    avatarUrl: 'https://cdn.pixabay.com/photo/2016/11/18/23/38/child-1837375_960_7'
+  },
+  {
+    id: 3,
+    name: 'Julio Fragoso',
+    email: 'juliafral@empmail.com',
+    avatarUrl: 'https://cdn.pixabay.com/photo/2016/11/18/23/38/child-1837375_960_7'
+  },
+  {
+    id: 4,
+    name: 'Rafael Monteiro',
+    email: 'rafamontee@empmail.com',
+    avatarUrl: 'https://cdn.pixabay.com/photo/2016/11/18/23/38/child-1837375_960_7'
+  }
+]
