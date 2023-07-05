313ab36a4982ac05393ca6583f1f002d7375d501
Vinicius Gabriel <vinihbieel49@gmail.com>
2023-07-05 19:01:11 -0300

    Bottom tab navigation

--- App.js
+++ App.js
@@ -3,20 +3,13 @@ import Header from './components/Header'
 import { View } from 'react-native'
 import Post from './components/Post'
 import Feed from './screens/Feed'
+import MenuNavigator from './Navigator'
 
 export default class App extends Component {
   render() {
-    const comments = [{
-      nickname: 'Joana Elena Silva',
-      comment: 'Excelente Foto'
-    }, {
-      nickname: 'Rafael Gustavo Pereira',
-      comment: 'Muito Ruim!'
-    }]
-
     return (
       <View style={{ flex: 1 }}>
-        <Feed />
+        <MenuNavigator />
       </View>
     )
   }
---
+++ Navigator.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import Icon from 'react-native-vector-icons/FontAwesome'
+import { NavigationContainer } from '@react-navigation/native';
+
+import Feed from './screens/Feed'
+
+const Tab = createBottomTabNavigator();
+
+export default function MenuNavigator() {
+  return (
+    <NavigationContainer >
+      <Tab.Navigator screenOptions={{ headerShown: false }}>
+        <Tab.Screen name="Home" component={Feed}
+          options={{
+            tabBarShowLabel: false,
+            tabBarIcon: ({ color }) => (
+              <Icon name='home' size={30} color={color} />
+            ),
+          }} />
+        <Tab.Screen name="AddPhoto" component={Feed}
+          options={{
+            tabBarShowLabel: false,
+            tabBarIcon: ({ color }) => (
+              <Icon name='camera' size={30} color={color} />
+            ),
+          }} />
+        <Tab.Screen name="Profile" component={Feed}
+          options={{
+            tabBarShowLabel: false,
+            tabBarIcon: ({ color }) => (
+              <Icon name='user' size={30} color={color} />
+            ),
+          }} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+}
